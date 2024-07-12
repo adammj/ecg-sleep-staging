Remove commented-out code and fix midnight_offset calculation.

your_own_data/primary_model/sleepnet.py
@@ -135,9 +135,9 @@ def sleepnet_collate(batch: list) -> dict:
         night = (night / ((sample["epoch_count"] - 1.0) / 2.0)) - 1.0
         night = night.unsqueeze(1)
 
-        # create midnight (-1=noon, 0=midnight, 1=noon) vectors
+        # create midnight (0=midnight, +/-1=+/-24hrs) vectors
         midnight_vec = torch.tensor(range(0, sample["epoch_count"]))
-        midnight_vec = midnight_vec / ((3600 / 30) * 12) + sample["midnight_offset"]
+        midnight_vec = midnight_vec / ((3600 / 30) * 24) + sample["midnight_offset"]
         midnight_vec = midnight_vec.unsqueeze(1)
 
         # concat the the demographics, with night and midnight