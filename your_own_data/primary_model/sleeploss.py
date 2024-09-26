adammj/ecg-sleep-staging

commit 262bedae724ae54d91d960ad77945bed5fa42328

Minor tweaks and memory improvements.

your_own_data/primary_model/sleeploss.py
@@ -1,22 +1,20 @@
 # Copyright (C) 2024  Adam Jones  All Rights Reserved
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
 # by the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-""" This file is just for the SleepLoss class.
-"""
-
+"""This file is just for the SleepLoss class."""
 
 import torch
 from torch import Tensor
@@ -81,26 +79,26 @@ class SleepLoss(_WeightedLoss):
         return final_loss
 
     def forward(
-        self, input_stages: Tensor, target_stages: Tensor, epoch_weights: Tensor
+        self, input: Tensor, target_stages: Tensor, epoch_weights: Tensor
     ) -> Tensor:
         """pytorch forward call"""
 
         # copy the weights across rows
-        epoch_weights = epoch_weights.unsqueeze(1).repeat(1, self.stage_count)
+        epoch_weights = epoch_weights.unsqueeze(1).expand(1, self.stage_count)
 
         # multiply the input by the weights
         # this will automatically ignore bad epochs (with weight = 0)
-        input_stages = input_stages * epoch_weights
+        input *= epoch_weights
 
         # build confusion from each target stage
         # this will automatically ignore any padded stages (with target = -1)
         overall_confusion = torch.zeros(
-            (5, 5), dtype=input_stages.dtype, device=input_stages.device
+            (self.stage_count, self.stage_count),
+            dtype=input.dtype,
+            device=input.device,
         )
         for i in range(self.stage_count):
-            overall_confusion[i, :] += input_stages[target_stages == i].sum(
-                0
-            )
+            overall_confusion[i, :] += input[target_stages == i].sum(0)
 
         # store a copy that can be used later.
         self.loss_confusion = overall_confusion.detach().clone()

your_own_data/primary_model/sleepnet.py
@@ -624,7 +624,7 @@ class SleepNet(Module):
 
         # zero out the features for the padded epochs
         # this is to prevent modifying the feature layers for padded epochs
-        features_out = out * padding_eliminator.repeat(
+        features_out = out * padding_eliminator.expand(
             1, 1, out.size(2)
         )  # expand to number of features
 

your_own_data/primary_model_no_demographics/sleeploss.py
@@ -1,22 +1,20 @@
 # Copyright (C) 2024  Adam Jones  All Rights Reserved
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
 # by the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-""" This file is just for the SleepLoss class.
-"""
-
+"""This file is just for the SleepLoss class."""
 
 import torch
 from torch import Tensor
@@ -81,26 +79,26 @@ class SleepLoss(_WeightedLoss):
         return final_loss
 
     def forward(
-        self, input_stages: Tensor, target_stages: Tensor, epoch_weights: Tensor
+        self, input: Tensor, target_stages: Tensor, epoch_weights: Tensor
     ) -> Tensor:
         """pytorch forward call"""
 
         # copy the weights across rows
-        epoch_weights = epoch_weights.unsqueeze(1).repeat(1, self.stage_count)
+        epoch_weights = epoch_weights.unsqueeze(1).expand(1, self.stage_count)
 
         # multiply the input by the weights
         # this will automatically ignore bad epochs (with weight = 0)
-        input_stages = input_stages * epoch_weights
+        input *= epoch_weights
 
         # build confusion from each target stage
         # this will automatically ignore any padded stages (with target = -1)
         overall_confusion = torch.zeros(
-            (5, 5), dtype=input_stages.dtype, device=input_stages.device
+            (self.stage_count, self.stage_count),
+            dtype=input.dtype,
+            device=input.device,
         )
         for i in range(self.stage_count):
-            overall_confusion[i, :] += input_stages[target_stages == i].sum(
-                0
-            )
+            overall_confusion[i, :] += input[target_stages == i].sum(0)
 
         # store a copy that can be used later.
         self.loss_confusion = overall_confusion.detach().clone()

your_own_data/primary_model_no_demographics/sleepnet.py
@@ -624,7 +624,7 @@ class SleepNet(Module):
 
         # zero out the features for the padded epochs
         # this is to prevent modifying the feature layers for padded epochs
-        features_out = out * padding_eliminator.repeat(
+        features_out = out * padding_eliminator.expand(
             1, 1, out.size(2)
         )  # expand to number of features
 

your_own_data/real-time_model/sleeploss.py
@@ -1,22 +1,20 @@
 # Copyright (C) 2024  Adam Jones  All Rights Reserved
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
 # by the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-""" This file is just for the SleepLoss class.
-"""
-
+"""This file is just for the SleepLoss class."""
 
 import torch
 from torch import Tensor
@@ -81,26 +79,26 @@ class SleepLoss(_WeightedLoss):
         return final_loss
 
     def forward(
-        self, input_stages: Tensor, target_stages: Tensor, epoch_weights: Tensor
+        self, input: Tensor, target_stages: Tensor, epoch_weights: Tensor
     ) -> Tensor:
         """pytorch forward call"""
 
         # copy the weights across rows
-        epoch_weights = epoch_weights.unsqueeze(1).repeat(1, self.stage_count)
+        epoch_weights = epoch_weights.unsqueeze(1).expand(1, self.stage_count)
 
         # multiply the input by the weights
         # this will automatically ignore bad epochs (with weight = 0)
-        input_stages = input_stages * epoch_weights
+        input *= epoch_weights
 
         # build confusion from each target stage
         # this will automatically ignore any padded stages (with target = -1)
         overall_confusion = torch.zeros(
-            (5, 5), dtype=input_stages.dtype, device=input_stages.device
+            (self.stage_count, self.stage_count),
+            dtype=input.dtype,
+            device=input.device,
         )
         for i in range(self.stage_count):
-            overall_confusion[i, :] += input_stages[target_stages == i].sum(
-                0
-            )
+            overall_confusion[i, :] += input[target_stages == i].sum(0)
 
         # store a copy that can be used later.
         self.loss_confusion = overall_confusion.detach().clone()

your_own_data/real-time_model/sleepnet.py
@@ -625,7 +625,7 @@ class SleepNet(Module):
 
         # zero out the features for the padded epochs
         # this is to prevent modifying the feature layers for padded epochs
-        features_out = out * padding_eliminator.repeat(
+        features_out = out * padding_eliminator.expand(
             1, 1, out.size(2)
         )  # expand to number of features
 

commit 5089bc15fb52898f8a4d36452d9b6feb429f4a6c

Fix regression caused by 262beda. Switching '.expand()' back to '.repeat()' in both the loss function and sleepnet. Tested fix verified in all 3 models.

your_own_data/primary_model/sleeploss.py
@@ -84,7 +84,7 @@ class SleepLoss(_WeightedLoss):
         """pytorch forward call"""
 
         # copy the weights across rows
-        epoch_weights = epoch_weights.unsqueeze(1).expand(1, self.stage_count)
+        epoch_weights = epoch_weights.unsqueeze(1).repeat(1, self.stage_count)
 
         # multiply the input by the weights
         # this will automatically ignore bad epochs (with weight = 0)

your_own_data/primary_model/sleepnet.py
@@ -624,9 +624,9 @@ class SleepNet(Module):
 
         # zero out the features for the padded epochs
         # this is to prevent modifying the feature layers for padded epochs
-        features_out = out * padding_eliminator.expand(
+        features_out = out * padding_eliminator.repeat(
             1, 1, out.size(2)
-        )  # expand to number of features
+        )  # repeat to number of features
 
         # permute to (subjects, features, epochs)
         out = features_out.permute(0, 2, 1)

your_own_data/primary_model_no_demographics/sleeploss.py
@@ -84,7 +84,7 @@ class SleepLoss(_WeightedLoss):
         """pytorch forward call"""
 
         # copy the weights across rows
-        epoch_weights = epoch_weights.unsqueeze(1).expand(1, self.stage_count)
+        epoch_weights = epoch_weights.unsqueeze(1).repeat(1, self.stage_count)
 
         # multiply the input by the weights
         # this will automatically ignore bad epochs (with weight = 0)

your_own_data/primary_model_no_demographics/sleepnet.py
@@ -624,9 +624,9 @@ class SleepNet(Module):
 
         # zero out the features for the padded epochs
         # this is to prevent modifying the feature layers for padded epochs
-        features_out = out * padding_eliminator.expand(
+        features_out = out * padding_eliminator.repeat(
             1, 1, out.size(2)
-        )  # expand to number of features
+        )  # repeat to number of features
 
         # permute to (subjects, features, epochs)
         out = features_out.permute(0, 2, 1)

your_own_data/real-time_model/sleeploss.py
@@ -84,7 +84,7 @@ class SleepLoss(_WeightedLoss):
         """pytorch forward call"""
 
         # copy the weights across rows
-        epoch_weights = epoch_weights.unsqueeze(1).expand(1, self.stage_count)
+        epoch_weights = epoch_weights.unsqueeze(1).repeat(1, self.stage_count)
 
         # multiply the input by the weights
         # this will automatically ignore bad epochs (with weight = 0)

your_own_data/real-time_model/sleepnet.py
@@ -625,9 +625,9 @@ class SleepNet(Module):
 
         # zero out the features for the padded epochs
         # this is to prevent modifying the feature layers for padded epochs
-        features_out = out * padding_eliminator.expand(
+        features_out = out * padding_eliminator.repeat(
             1, 1, out.size(2)
-        )  # expand to number of features
+        )  # repeat to number of features
 
         # permute to (subjects, features, epochs)
         out = features_out.permute(0, 2, 1)